551b1524393475e11ca285c4e6d484b069387ea1
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-07-24 04:32:08 +0200

    update for parrot 3.1

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ function build() {
 	lb clean
 	lb config
 	lb build &&\
-	mv live-image-*.hybrid.iso Parrot-$variant-$version\_$arch.iso
+	mv live-image-*.hybrid.iso ../Parrot-$variant-$version\_$arch.iso
 }
 
 case $1 in
