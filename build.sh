98b184885395f928d71425f815d91bc97a8a680c
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-07-24 20:14:11 +0200

    remove veil references

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ EXPLAINATION
                 written in the live boot menu
                  
 EXAMPLE
- 	 ./build.sh build standard amd64 3.0-CUSTOM
+ 	 ./build.sh build standard amd64 3.1-CUSTOM
 "
 }
 
